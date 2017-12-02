c4ed32e60e90c1e5209cdf29456e3cb833b3238c
Pelikandr <33723523+Pelikandr@users.noreply.github.com>
2017-12-03 01:24:39 +0200

    загружены формы разметок, машины и задний фон

---
+++ nfs/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace nfs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.rm11 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.mainCar = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rm12 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm13 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm14 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm15 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm21 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm31 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm41 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm25 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm24 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm23 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm22 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm35 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm34 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm33 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm32 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm45 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm44 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm43 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.rm42 = new Microsoft.VisualBasic.PowerPacks.RectangleShape();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.rm42,
+            this.rm43,
+            this.rm44,
+            this.rm45,
+            this.rm32,
+            this.rm33,
+            this.rm34,
+            this.rm35,
+            this.rm22,
+            this.rm23,
+            this.rm24,
+            this.rm25,
+            this.rm41,
+            this.rm31,
+            this.rm21,
+            this.rm15,
+            this.rm14,
+            this.rm13,
+            this.rm12,
+            this.rm11,
+            this.mainCar});
+            this.shapeContainer1.Size = new System.Drawing.Size(1362, 741);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // rm11
+            // 
+            this.rm11.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm11.BorderColor = System.Drawing.Color.Transparent;
+            this.rm11.Location = new System.Drawing.Point(454, 603);
+            this.rm11.Name = "rm11";
+            this.rm11.Size = new System.Drawing.Size(10, 156);
+            // 
+            // mainCar
+            // 
+            this.mainCar.BackgroundImage = global::nfs.Properties.Resources.mainCar;
+            this.mainCar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mainCar.BorderColor = System.Drawing.Color.Red;
+            this.mainCar.Location = new System.Drawing.Point(327, 555);
+            this.mainCar.Name = "mainCar";
+            this.mainCar.Size = new System.Drawing.Size(111, 181);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1141, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rm12
+            // 
+            this.rm12.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm12.BorderColor = System.Drawing.Color.Transparent;
+            this.rm12.Location = new System.Drawing.Point(454, 447);
+            this.rm12.Name = "rm12";
+            this.rm12.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm13
+            // 
+            this.rm13.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm13.BorderColor = System.Drawing.Color.Transparent;
+            this.rm13.Location = new System.Drawing.Point(454, 290);
+            this.rm13.Name = "rm13";
+            this.rm13.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm14
+            // 
+            this.rm14.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm14.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm14.BorderColor = System.Drawing.Color.Transparent;
+            this.rm14.Location = new System.Drawing.Point(454, 134);
+            this.rm14.Name = "rm14";
+            this.rm14.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm15
+            // 
+            this.rm15.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm15.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm15.BorderColor = System.Drawing.Color.Transparent;
+            this.rm15.Location = new System.Drawing.Point(454, -22);
+            this.rm15.Name = "rm15";
+            this.rm15.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm21
+            // 
+            this.rm21.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm21.BorderColor = System.Drawing.Color.Transparent;
+            this.rm21.Location = new System.Drawing.Point(600, 603);
+            this.rm21.Name = "rm21";
+            this.rm21.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm31
+            // 
+            this.rm31.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm31.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm31.BorderColor = System.Drawing.Color.Transparent;
+            this.rm31.Location = new System.Drawing.Point(746, 603);
+            this.rm31.Name = "rm31";
+            this.rm31.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm41
+            // 
+            this.rm41.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm41.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm41.BorderColor = System.Drawing.Color.Transparent;
+            this.rm41.Location = new System.Drawing.Point(892, 603);
+            this.rm41.Name = "rm41";
+            this.rm41.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm25
+            // 
+            this.rm25.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm25.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm25.BorderColor = System.Drawing.Color.Transparent;
+            this.rm25.Location = new System.Drawing.Point(600, -22);
+            this.rm25.Name = "rm25";
+            this.rm25.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm24
+            // 
+            this.rm24.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm24.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm24.BorderColor = System.Drawing.Color.Transparent;
+            this.rm24.Location = new System.Drawing.Point(600, 134);
+            this.rm24.Name = "rm24";
+            this.rm24.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm23
+            // 
+            this.rm23.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm23.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm23.BorderColor = System.Drawing.Color.Transparent;
+            this.rm23.Location = new System.Drawing.Point(600, 290);
+            this.rm23.Name = "rm23";
+            this.rm23.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm22
+            // 
+            this.rm22.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm22.BorderColor = System.Drawing.Color.Transparent;
+            this.rm22.Location = new System.Drawing.Point(600, 447);
+            this.rm22.Name = "rm22";
+            this.rm22.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm35
+            // 
+            this.rm35.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm35.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm35.BorderColor = System.Drawing.Color.Transparent;
+            this.rm35.Location = new System.Drawing.Point(746, -21);
+            this.rm35.Name = "rm35";
+            this.rm35.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm34
+            // 
+            this.rm34.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm34.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm34.BorderColor = System.Drawing.Color.Transparent;
+            this.rm34.Location = new System.Drawing.Point(746, 135);
+            this.rm34.Name = "rm34";
+            this.rm34.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm33
+            // 
+            this.rm33.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm33.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm33.BorderColor = System.Drawing.Color.Transparent;
+            this.rm33.Location = new System.Drawing.Point(746, 291);
+            this.rm33.Name = "rm33";
+            this.rm33.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm32
+            // 
+            this.rm32.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm32.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm32.BorderColor = System.Drawing.Color.Transparent;
+            this.rm32.Location = new System.Drawing.Point(746, 448);
+            this.rm32.Name = "rm32";
+            this.rm32.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm45
+            // 
+            this.rm45.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm45.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm45.BorderColor = System.Drawing.Color.Transparent;
+            this.rm45.Location = new System.Drawing.Point(892, -22);
+            this.rm45.Name = "rm45";
+            this.rm45.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm44
+            // 
+            this.rm44.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm44.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm44.BorderColor = System.Drawing.Color.Transparent;
+            this.rm44.Location = new System.Drawing.Point(892, 134);
+            this.rm44.Name = "rm44";
+            this.rm44.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm43
+            // 
+            this.rm43.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm43.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm43.BorderColor = System.Drawing.Color.Transparent;
+            this.rm43.Location = new System.Drawing.Point(892, 290);
+            this.rm43.Name = "rm43";
+            this.rm43.Size = new System.Drawing.Size(10, 156);
+            // 
+            // rm42
+            // 
+            this.rm42.BackgroundImage = global::nfs.Properties.Resources.roadMarking;
+            this.rm42.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rm42.BorderColor = System.Drawing.Color.Transparent;
+            this.rm42.Location = new System.Drawing.Point(892, 447);
+            this.rm42.Name = "rm42";
+            this.rm42.Size = new System.Drawing.Size(10, 156);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::nfs.Properties.Resources.bg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape mainCar;
+        private System.Windows.Forms.Button button1;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm11;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm15;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm14;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm13;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm12;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm41;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm31;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm21;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm42;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm43;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm44;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm45;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm32;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm33;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm34;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm35;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm22;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm23;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm24;
+        private Microsoft.VisualBasic.PowerPacks.RectangleShape rm25;
+    }
+}
+
