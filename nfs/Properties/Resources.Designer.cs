adb7564d9568c3bff8d6500b0c62b9f777a45f9d
Pelikandr <33723523+Pelikandr@users.noreply.github.com>
2017-12-03 13:33:35 +0200

    no message

--- nfs/Properties/Resources.Designer.cs
+++ nfs/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace nfs.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap volna1 {
+            get {
+                object obj = ResourceManager.GetObject("volna1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
